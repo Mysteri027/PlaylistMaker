817791e85fc776920f46c8a4624f637cbaabcc37
Igor <igor.sheplyakov@yandex.ru>
2022-11-18 20:57:36 +0300

    first homework

---
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.playlistmaker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val searchButton = findViewById<Button>(R.id.search_button)
+        val mediaLibraryButton = findViewById<Button>(R.id.media_library_button)
+        val settingButton = findViewById<Button>(R.id.settings_button)
+
+        searchButton.setOnClickListener {
+            val searchIntent = Intent(this, SearchActivity::class.java)
+            startActivity(searchIntent)
+        }
+
+        mediaLibraryButton.setOnClickListener {
+            val mediaLibraryIntent = Intent(this, MediaLibraryActivity::class.java)
+            startActivity(mediaLibraryIntent)
+        }
+
+
+        settingButton.setOnClickListener {
+            val settingsIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingsIntent)
+        }
+
+    }
+}
