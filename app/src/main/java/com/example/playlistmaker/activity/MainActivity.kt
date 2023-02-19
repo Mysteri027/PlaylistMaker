fbc4bc0d80199bd4f3b9cdb5e77218467fc8c51b
Igor <igor.sheplyakov@yandex.ru>
2023-02-19 13:32:57 +0300

    refactoring

--- app/src/main/java/com/example/playlistmaker/activity/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/activity/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.playlistmaker.acrivity
+package com.example.playlistmaker.activity
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
--- app/src/main/java/com/example/playlistmaker/activity/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/activity/SearchActivity.kt
@@ -1,4 +1,4 @@
-package com.example.playlistmaker.acrivity
+package com.example.playlistmaker.activity
 
 import android.os.Bundle
 import android.text.Editable
